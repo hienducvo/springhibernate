6b90db914e3556b2dd3c182630dd551102ec853e
hienducvo <hiendvosf@gmail.com>
2017-08-06 10:52:03 -0500

    Add new sources

---
+++ src/main/java/edu/mum/cs/cs544/examples/dao/CustomerDAOImpl.java
@@ -0,0 +1,45 @@
+package edu.mum.cs.cs544.examples.dao;
+
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.mum.cs.cs544.examples.entity.Country;
+import edu.mum.cs.cs544.examples.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+	
+	@Autowired
+    SessionFactory sessionFactory;
+    
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+    
+    @SuppressWarnings("unchecked")
+	@Transactional
+	public List<Customer> getAllCustomers() {
+       // Query
+        Query query = sessionFactory.getCurrentSession().createQuery("from Customer c");
+        query.setParameter("countryName", "United States");
+
+		return query.list();
+	}
+
+    @Transactional
+    @SuppressWarnings("unchecked")
+    public List<Customer> getCustomerList(Country country) {
+        // Query
+        Query query = sessionFactory.getCurrentSession().createQuery("from Customer c where c.address.city.country.name = :countryName");
+        query.setParameter("countryName", "United States");
+		List<Customer> customers = query.list();
+
+		return customers;
+	}
+}
